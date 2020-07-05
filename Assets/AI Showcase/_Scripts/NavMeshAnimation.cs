e75dcbe7c9e8d27b16c977bba36c351c8fc3509e
Dan Domingues <dom.dev.contact@gmail.com>
2020-07-05 19:00:17 -0300

    Implemented Patrol scene. Added several assets.

---
+++ Assets/AI Showcase/_Scripts/NavMeshAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(Animator))]
+public class NavMeshAnimation : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator animator;
+    [SerializeField] float maxSpeed;
+
+    private void Reset()
+    {
+        agent = GetComponentInParent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+        if (agent) maxSpeed = agent.speed;
+    }
+
+    private void FixedUpdate()
+    {
+        animator.SetFloat("MoveSpeed", agent.velocity.magnitude / maxSpeed);
+    }
+}
